8eee3a23705b4501ba06af8f71651770d938823e
ShuaiNiu01 <Shuai.Niu@dal.ca>
2021-07-17 01:29:01 -0300

    first commit

--- calculator.js
+++ calculator.js
@@ -7,6 +7,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/bmicalculator", function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req,res){
 
   var num1 = Number(req.body.n1);
@@ -14,6 +19,15 @@ app.post("/", function(req,res){
   var result = num1 + num2;
 
   res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmicalculator", function(req,res){
+
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height)/100;
+  var bmi = weight/Math.pow(height, 2);
+
+  res.send("Your BMI is " + Math.floor(bmi*10)/10);
 });
 
 app.listen(3000, function(){
